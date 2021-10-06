26510cf21235dc26b7303d07b9c6b0c057af4542
WefPok <nik4zerg@gmail.com>
2021-10-06 18:38:12 +0300

    Course Manager added

--- src/moodle/Course.java
+++ src/moodle/Course.java
@@ -3,7 +3,23 @@ package moodle;
 import java.util.List;
 
 public class Course {
+    public String getInstructor() {
+        return instructor;
+    }
 
+    public List<String> getStudents() {
+        return students;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    Course(String instructor, List<String> students, String name){
+        this.instructor = instructor;
+        this.students = students;
+        this.name = name;
+    }
     private String instructor;
     private List<String> students;
     private String name;
--- src/moodle/CourseManager.java
+++ src/moodle/CourseManager.java
@@ -1,14 +1,54 @@
 package moodle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class CourseManager {
     static CourseManager INSTANCE = new CourseManager();
     private CourseManager() {}
 
-    Course createCourse(Instructor instructor, List<String> students, String name){
+    private List<Course> courses;
 
+    Course createCourse(String instructor, List<String> students, String name){
+        return new Course(instructor, students, name);
+    }
+    List<Course> getCourses(){
+        return this.courses;
+    }
+
+    List<Course> getCoursesByInstructor(String instructor){
+        List<Course> temp = new ArrayList<>();
+        for (Course course:
+             courses) {
+            if (course.getInstructor().equals(instructor)){
+                temp.add(course);
+            }
+        }
+        return temp;
+    }
+
+    List<Course> getCoursesByStudent(String student){
+        List<Course> temp = new ArrayList<>();
+        for (Course course:
+                courses) {
+            if (course.getStudents().contains(student)){
+                temp.add(course);
+            }
+        }
+        return temp;
+    }
+
+    Course getCourseByName(String name){
+        for (Course course:
+                courses) {
+            if (course.getName().equals(name)){
+                return course;
+            }
+        }
         return null;
     }
-    List<Course> getCourses()
+
+    void removeCourse(String name){
+        courses.removeIf(course -> course.getName().equals(name));
+    }
 }
