8402c8831a0612247b4432f1207f18d68a76241e
WefPok <nik4zerg@gmail.com>
2021-10-06 18:03:01 +0300

    Admin added

---
+++ src/moodle/Admin.java
@@ -0,0 +1,45 @@
+package moodle;
+
+import java.util.List;
+
+public class Admin implements Module{
+    // -------------Creators---------------
+    public Student createStudent(String email, String password){
+
+        return null;
+    }
+    public Instructor createInstructor(String email, String password){
+
+        return null;
+    }
+    public Course createCourse(String name, String instructorEmail, List<String> students){
+
+        return null;
+    }
+
+    // -------------Getters---------------
+    public Stusent getStudent(String email){
+
+        return null;
+    }
+    public Instructor getInstructor(String email){
+
+        return null;
+    }
+    public Course getCourse(String name){
+
+        return null;
+    }
+
+    // -------------Removers---------------
+    public void removeStudent(String email){
+
+    }
+    public void removeInstructor(String email){
+
+    }
+    public void removeCourse(String name){
+
+    }
+
+}
